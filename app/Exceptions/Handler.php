a829bfc09d711a86e26289f60af1a73245603b7c
saif hesham <saif2nemr@gmail.com>
2019-12-29 13:35:57 +0200

    first update for laravel RESTful api

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Exceptions;
+
+use App\Traits\ApiResponse;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+
+class Handler extends ExceptionHandler
+{
+    use ApiResponse;
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * @param  \Exception  $exception
+     * @return void
+     */
+    public function report(Exception $exception)
+    {
+        
+        parent::report($exception);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $exception)
+    {
+        if($exception instanceof ValidationException )
+            return $this->convertValidationExceptionToResponse();
+        if($exception instanceof ModelNotFoundException){
+            $modelName = $exception->getModel();
+            //return 'home '.$modelName;
+            return $this->errorResponse('This model [ '.$modelName.' ] not found in this model',404);
+        }
+        if($exception instanceof AuthorizationException){
+            $this->errorResponse($exception->getMessage(),403);
+        }
+        if($exception instanceof NotFoundHttpException)
+            return $this->errorResponse('This url not found!',404);
+        if($exception instanceof MethodNotAllowedHttpException){
+            return $this->errorResponse('This d found!',404);
+        }
+        return parent::render($request, $exception);
+    }
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
+    {
+        //return 'error here';
+        if ($e->response) {
+            return $e->response;
+        }
+
+        return $request->expectsJson()
+                    ? $this->invalidJson($request, $e)
+                    : $this->invalid($request, $e);
+    }
+}
